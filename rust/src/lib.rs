69531b14267a88f23b77475cadaf8cbaca1c4113
messenger_qs <quadrat.ik@yandex.by>
2024-08-29 12:43:59 +0300

    Add explicit type annotations

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,5 @@
 use std::{ffi::CString, panic};
+use std::panic::PanicHookInfo;
 use c_func::*;
 
 mod c_func;
@@ -6,9 +7,9 @@ mod c_func;
 #[allow(non_snake_case)]
 #[no_mangle]
 pub fn RS_Main() {
-    panic::set_hook(Box::new(|x| {
+    panic::set_hook(Box::new(|x: &PanicHookInfo| {
         unsafe {
-            let string = CString::new(x.to_string()).unwrap();
+            let string: CString = CString::new(x.to_string()).unwrap();
 
             COM_SetComErrno(-1);
             COM_DieNoFormat(string.as_ptr());
